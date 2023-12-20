a7ec9e901b6d4e2f23e204393d5861199ca198c0
Willian <williamhernandez450@gmail.com>
2023-12-20 11:15:46 -0600

    PostMaping in Spring MVC

--- src/main/java/com/whernandez/introthymeleaf/controller/HelloWorldController.java
+++ src/main/java/com/whernandez/introthymeleaf/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.whernandez.introthymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -43,7 +44,7 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
-	@GetMapping("/processFormVersionThree")
+	@PostMapping("/processFormVersionThree")
 	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
 		
 		// convert the data to all caps
