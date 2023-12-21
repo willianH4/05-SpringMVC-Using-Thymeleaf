160c4adc066ab59b7547d978a475f1b6478d4e15
Willian <williamhernandez450@gmail.com>
2023-12-20 19:25:49 -0600

    Drop down list or Select in Spring MVC

--- src/main/java/com/whernandez/introthymeleaf/controller/StudentController.java
+++ src/main/java/com/whernandez/introthymeleaf/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.whernandez.introthymeleaf.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +13,9 @@ import com.whernandez.introthymeleaf.model.Student;
 
 @Controller
 public class StudentController {
+	
+	@Value("${countries}")
+	private List<String> countries;
 
 	@GetMapping("/showStudentForm")
 	public String showForm(Model model) {
@@ -19,6 +25,9 @@ public class StudentController {
 		
 		// Add a student object to the model
 		model.addAttribute("student", theStudent);
+		
+		// add the list of countries to the model
+		model.addAttribute("countries", countries);
 		
 		return "student-form";
 		
