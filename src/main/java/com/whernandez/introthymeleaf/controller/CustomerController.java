a67892493d58465678c677ef1ea814e11f032896
Willian <williamhernandez450@gmail.com>
2024-01-02 20:06:12 -0600

    Spring MVC Validation with @InitBinder for validate whites spaces

--- src/main/java/com/whernandez/introthymeleaf/controller/CustomerController.java
+++ src/main/java/com/whernandez/introthymeleaf/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.whernandez.introthymeleaf.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.whernandez.introthymeleaf.model.Customer;
@@ -12,6 +15,19 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+	
+	// add an initbinder ... to convert trim input strings
+	// remove leading and trailing whitespaces
+	// resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+		
+	}
 
 	@GetMapping("/")
 	public String showForm(Model theModel) {
@@ -26,6 +42,8 @@ public class CustomerController {
 	public String processCustomerForm(
 			@Valid Customer theCustomer, 
 			BindingResult theBindingResult) {
+		
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 		
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
