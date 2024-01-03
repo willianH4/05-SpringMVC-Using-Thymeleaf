cbe83732a523bae496a6df7950fc1c571b2e1eb5
Willian <williamhernandez450@gmail.com>
2024-01-03 11:55:50 -0600

    Spring MVC Validation - Applying Regular expressions

--- src/main/java/com/whernandez/introthymeleaf/model/Customer.java
+++ src/main/java/com/whernandez/introthymeleaf/model/Customer.java
@@ -3,6 +3,7 @@ package com.whernandez.introthymeleaf.model;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
@@ -17,23 +18,39 @@ public class Customer {
 	@Max(value=10, message="must be less than or equal to 10")
 	private int freePasses;
 	
+	@Pattern(regexp = "^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
+	
 	public String getFirstName() {
 		return firstName;
 	}
+	
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
 	public String getLastName() {
 		return lastName;
 	}
+	
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
 	public int getFreePasses() {
 		return freePasses;
 	}
+	
 	public void setFreePasses(int freePasses) {
 		this.freePasses = freePasses;
+	}
+	
+	public String getPostalCode() {
+		return postalCode;
+	}
+	
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 }
